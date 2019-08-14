85df8ecf2730ce9d0892a0bef6aa7ccfdb15509b
Valentyn Chupa <mega-dmitro.chupa@ukr.net>
2019-08-14 21:09:05 +0300

    Task1 Part3

--- Task1/Part3/Circle/Circle.cs
+++ Task1/Part3/Circle/Circle.cs
@@ -3,20 +3,22 @@
     class Circle
     {
         private double pi=3.14159265359;
+
         private  double radius;
         public  double Radius
         {
             get { return radius; }
             set { radius = value; }
         }
+
         public  double Lenght(double radius)
         {
             return 2 * pi * radius;
         }
+
         public  double Perimeter(double radius)
         {
             return radius * pi * pi;
         }
-    
-}
+    }
 }
