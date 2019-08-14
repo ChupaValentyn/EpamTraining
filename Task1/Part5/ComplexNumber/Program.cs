400ccb615c50828f91f0eef5b72b1de44c696675
Valentyn Chupa <mega-dmitro.chupa@ukr.net>
2019-08-14 23:04:59 +0300

    add part5

---
+++ Task1/Part5/ComplexNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ComplexNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumbers NumbOne = new ComplexNumbers(0,0);
+
+            Console.Write("Enter real part on first number:");
+            NumbOne.Real = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter imaginary part on first number:");
+            NumbOne.Imaginary = Convert.ToInt32(Console.ReadLine());
+
+            ComplexNumbers NumbTwo = new ComplexNumbers(0, 0);
+
+            Console.Write("Enter real part on second number:");
+            NumbTwo.Real = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter imaginary part on second number:");
+            NumbTwo.Imaginary = Convert.ToInt32(Console.ReadLine());
+
+            ComplexNumbers rezult = new ComplexNumbers(0, 0);
+
+            rezult = NumbOne * NumbTwo;
+            rezult.GetInfo(rezult);
+
+            rezult = NumbOne / NumbTwo;
+            rezult.GetInfo(rezult);
+        }
+    }
+}
