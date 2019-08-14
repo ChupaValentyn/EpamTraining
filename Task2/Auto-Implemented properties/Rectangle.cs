40daf9876944026286207d6c5ec678d5278dc816
Valentyn Chupa <mega-dmitro.chupa@ukr.net>
2019-08-14 20:21:39 +0300

    Task2

---
+++ Task2/Auto-Implemented properties/Rectangle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auto_Implemented_properties
+{
+    class Rectangle
+    {
+        private Coordinate firstp;
+        public Coordinate FirstP
+        {
+            get { return this.firstp; }
+            set { this.firstp = value; }
+        }
+        private Coordinate secondp;
+        public Coordinate SecondP
+        {
+            get { return this.secondp; }
+            set { this.secondp = value; }
+        }
+        public Rectangle(Coordinate firstp, Coordinate secondp)
+        {
+            this.firstp = firstp;
+            this.secondp = secondp;
+        }
+        public double Square(Rectangle rec)
+        {
+            double square = 0;
+            Coordinate rez = new Coordinate(0,0);
+
+            rez = this.SearchSite(rec);
+
+            square = rez.X * rez.Y;
+
+            if (square < 0)
+            {
+                square *= -1;
+            }
+
+            return square;
+        }
+
+        public double Perimeter(Rectangle rectangle)
+        {
+            double perimeter = 0;
+            Coordinate rez = new Coordinate(0, 0);
+            rez = this.SearchSite(rectangle);
+            
+            if (rez.Y < 0)
+            {
+                rez.Y *= -1;
+            }
+
+            if (rez.X < 0)
+            {
+               rez.X *= -1;
+            }
+
+            perimeter = (rez.X + rez.Y) * 2;
+
+            return perimeter;
+        }
+
+        private Coordinate SearchSite(Rectangle rec)
+        {
+            Coordinate fall = new Coordinate(0, 0);
+
+            fall = rec.firstp - rec.secondp;
+
+            return fall;
+        }
+    }
+}
